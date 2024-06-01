7e41389f0a566f4fff8b3f30ea95c40f2ff3a721
DeepakVelmurugan <deepazlions@gmail.com>
2024-06-01 22:16:31 +0530

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import useGenres from "../hooks/useGenres";
+
+const GenreGrid = () => {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreGrid;
